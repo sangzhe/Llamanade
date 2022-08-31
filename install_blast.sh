f30c42aabba4c58d444b652d03344a08f15152dd
SangZhe <zhe.sang@gmail.com>
2022-08-31 16:11:17 -0400

    Delete install_blast.sh

--- install_blast.sh
+++
@@ -1,8 +0,0 @@
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.11.0/ncbi-blast-2.11.0+-src.tar.gz
-tar -xzvf ncbi-blast-2.11.0+-src.tar.gz
-rm ncbi-blast-2.11.0+-src.tar.gz
-cd ncbi-blast-2.11.0+-src
-cd c++
-./configure
-cd ReleaseMT/build
-make all_r
